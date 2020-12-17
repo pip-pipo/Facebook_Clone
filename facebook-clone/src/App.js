9f213e1e9ac075a06ef9c7789c429d43819711f1
pip-pipo <codemi692@gmail>
2020-12-17 10:01:18 -1100

    Contribe For making your own changes

---
+++ facebook-clone/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header'
+function App() {
+  return (
+    <div className="App">
+     {/* header */}
+     <Header />
+     {/* body */}
+     {/* slider */}
+     {/* widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ facebook-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyALa7WEl6GGy85dNN8QgnTeL4rMPbQEJlE",
+    authDomain: "facebook-clone-b552d.firebaseapp.com",
+    projectId: "facebook-clone-b552d",
+    storageBucket: "facebook-clone-b552d.appspot.com",
+    messagingSenderId: "313608143607",
+    appId: "1:313608143607:web:8f435b34e2149974e449f2",
+    measurementId: "G-VPYTRG0QBZ"
+  };
