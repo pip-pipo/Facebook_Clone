1218900d649d5f0f52a098f8a3af5ba61865cfef
pip-pipo <codemi692@gmail>
2020-12-18 12:21:43 -1100

    :books:

--- facebook-clone/src/sidebar.js
+++ facebook-clone/src/sidebar.js
@@ -8,10 +8,7 @@ import ChatIcon from '@material-ui/icons/Chat';
 import StorefrontIcon from '@material-ui/icons/Storefront';
 import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
 import ExpandMoreOutlined from '@material-ui/icons/ExpandMoreOutlined';
-
 import {useStateValue} from './StateProvider';
-
-
 
 function Sidebar() {
 
